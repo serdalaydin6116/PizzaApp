c1923d717f87daa0cf2648d7e7f7640c1138e000
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-09-03 01:32:43 +0300

    forms and views  were studied

--- pizza/views.py
+++ pizza/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Size, Pizza
 
 # Create your views here.
+def home(request):
+    # todos = Todo.objects.all()
+    # form = TodoForm()
+    # context = {
+    #     "todos" : todos,
+    #     "form" : form
+    # }
+
+    return render(request, "pizza/home.html")
