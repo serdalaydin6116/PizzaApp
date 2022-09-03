f1bc74fedd0e04b1a5571003d4d6e09033b2ef0a
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-09-03 04:10:01 +0300

    synched with the updated models.py

--- pizza/urls.py
+++ pizza/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import home
-
+from .views import home, pizzas, order, edit_order
 
 urlpatterns=[
     path('', home, name='home'),
-    # path('add/', todo_create, name='add'),
-    # path('update/<int:id>', todo_update, name='update'),
+    path('list/', pizzas, name='list'),
+    path('order/', order, name='order'),
+    path('edit/<int:id>', edit_order, name='edit'),
     # path('delete/<int:id>', todo_delete, name='delete')
 
 
--- pizza/views.py
+++ pizza/views.py
@@ -1,14 +1,55 @@
-from django.shortcuts import render
+from multiprocessing import context
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Size, Pizza
+from .forms import PizzaForm
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
-    # todos = Todo.objects.all()
-    # form = TodoForm()
-    # context = {
-    #     "todos" : todos,
-    #     "form" : form
-    # }
+    return render(request,'pizza/home.html')
 
-    return render(request, "pizza/home.html")
+
+def pizzas(request):
+    pizzas=Pizza.objects.all()
+    
+    context = {
+        "pizzas" : pizzas,
+        
+    }
+    return render(request, "pizza/pizzas.html", context)
+
+
+
+def order(request):
+    form = PizzaForm()
+
+    if request.method == "POST":
+        form = PizzaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"Your order created successfully")
+            return redirect("list")
+
+    context = {
+        "form" : form
+    }
+
+    return render(request, "pizza/order.html", context)
+
+def edit_order(request, id):
+    pizza = Pizza.objects.get(id=id)
+    form = PizzaForm(instance=pizza)
+
+    if request.method == "POST":
+        form = PizzaForm(request.POST, instance=pizza)
+        if form.is_valid():
+            form.save()
+            return redirect("list")
+
+    context = {
+        "pizza" : pizza,
+        "form" : form
+    }
+
+    return render(request, "pizza/edit_order.html", context)
