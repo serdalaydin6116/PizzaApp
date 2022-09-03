d3940115187662082711b943ce932681e5bae0e8
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-09-03 15:43:51 +0300

    urls was updated

--- pizza/urls.py
+++ pizza/urls.py
@@ -3,7 +3,7 @@ from .views import home, pizzas, order, edit_order
 
 urlpatterns=[
     path('', home, name='home'),
-    path('list/', pizzas, name='list'),
+    path('pizzas/', pizzas, name='pizzas'),
     path('order/', order, name='order'),
     path('edit/<int:id>', edit_order, name='edit'),
     # path('delete/<int:id>', todo_delete, name='delete')
